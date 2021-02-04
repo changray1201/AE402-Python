51d395ff68bbf03fb3ba4b4649ee5e6addaa82e2
changray1201 <78331626+changray1201@users.noreply.github.com>
2021-02-04 11:53:11 +0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+alex = {
+    'height'  :175,
+    'age'     :14,
+    'school'  :'py',
+    'gender'  :'male',
+    'birthday':1021 
+    }
+
+for k in alex.keys() :
+    print(k)
+for v in alex.values() :
+    print(v)
+for k , v in alex.items() :
+    print(k , v)
